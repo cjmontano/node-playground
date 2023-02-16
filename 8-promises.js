075998d2f296d94bd5f085905f914d3236c55795
CJ Montano <cornel.montano@gmail.com>
2023-02-16 09:27:30 -0600

    updated promises and added async/await

--- 8-promises.js
+++ 8-promises.js
@@ -1,18 +1,17 @@
 //Comparison to callbacks 
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        //Can only call resolve or reject.  Can only call resolve/reject one time
-        // resolve([4, 12, 18]) // success
-        reject ('This is my error') //failure
-        // reject ('This is my error') //failure
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //Can only call resolve or reject.  Can only call resolve/reject one time
+//         resolve([4, 12, 18]) // success
+//         // reject ('This is my error') //failure
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-    console.log('Success:', result)
-}).catch((error) => {
-    console.log('Error:', error)
-})
+// doWorkPromise.then((result) => {
+//     console.log('Success:', result)
+// }).catch((error) => {
+//     console.log('Error:', error)
+// })
 
 //
 //                           fulfilled
@@ -20,3 +19,31 @@ doWorkPromise.then((result) => {
 // Promise  -- pending --->
 //                          \
 //                           rejected
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// add(1,3).then((sum) => {
+//     console.log(sum)
+//     add(2,11).then((sum2) => {
+//         console.log(sum2)
+//     }).catch((e) => {
+//         console.log("Error in inner add")
+//     })
+// }).catch((e) => {
+//     console.log('Error in outer add')
+// })
+
+add(1,3).then((sum) => {
+    console.log(sum)
+    return add(sum, 4) // This allows promise chaining
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
+})
